8910ac2a25e3564453910ccf208076914626f199
Kyle Nielsen <kwnielsen17@gmail.com>
2021-11-03 18:00:05 -0600

    First Commit

---
+++ src/mock-data.js
@@ -0,0 +1,358 @@
+let mock = [{
+    id: 1,
+    name: "Bugatti Veyron Super Sport",
+    price: "$2,400,000",
+    engine: "8.0L W16 Gas",
+    image: "veyron.jpg"
+  },
+  {
+    id: 2,
+    name: "Porsche Taycan Turbo S",
+    price: "$185,000",
+    engine: "Electric",
+    image: "taycan.jpg"
+  },
+  {
+    id: 3,
+    name: "Dodge Demon",
+    price: "$84,995",
+    engine: "6.2L V8 Gas",
+    image: "demon.jpg"
+  },
+  {
+    id: 4,
+    name: "Hennessey Mammoth 1000",
+    price: "$135,350",
+    engine: "6.2L V8 Gas",
+    image: "mammoth.jpg"
+  },
+  {
+    id: 5,
+    name: "Porsche 911",
+    price: "$101,200",
+    engine: "4.0L Flat-Six-Cylinder Gas",
+    image: "911.jpg"
+
+  },
+  {
+    id: 6,
+    name: "Lamborghini Aventador SVJ",
+    price: "$517,770",
+    engine: "6.5L V12 Gas",
+    image: "aventador-svj.jpg"
+  },
+  {
+    id: 7,
+    name: "Tesla Model 3",
+    price: "$43,990",
+    engine: "Electric",
+    image: "tesla.jpg"
+  },
+  {
+    id: 8,
+    name: "Cucumber",
+    price: "$3.02",
+    engine: "Canada",
+    image: "cucumber.jpg"
+  },
+  {
+    id: 9,
+    name: "Fava Beans",
+    price: "$4.76",
+    engine: "Brazil",
+    image: "fava-beans.jpg"
+
+  },
+  {
+    id: 10,
+    name: "Coconut",
+    price: "$2.50",
+    engine: "Brazil",
+    image: "coconut.jpg"
+  },
+  {
+    id: 11,
+    name: "Kettle Potato Chips",
+    price: "$6.70",
+    engine: "United States",
+    image: "potato-chips.jpg"
+  },
+  {
+    id: 12,
+    name: "Flour - Rye",
+    price: "$1.14",
+    engine: "Canada",
+    image: "rye-flour.jpg"
+  },
+  {
+    id: 13,
+    name: "Feta Chees",
+    price: "$6.92",
+    engine: "Brazil",
+    image: "feta-cheese.jpg"
+  },
+  {
+    id: 14,
+    name: "Cheddar Cheese",
+    price: "$5.36",
+    engine: "Mexico",
+    image: "cheddar-cheese.jpg"
+  },
+  {
+    id: 15,
+    name: "Capers",
+    price: "$4.18",
+    engine: "United States",
+    image: "capers.jpg"
+  },
+  {
+    id: 16,
+    name: "Baguette",
+    price: "$9.18",
+    engine: "Mexico",
+    image: "baguette.jpg"
+  },
+  {
+    id: 17,
+    name: "Lasagna Noodles",
+    price: "$4.73",
+    engine: "Brazil",
+    image: "lasagna-pasta.jpg"
+  },
+  {
+    id: 18,
+    name: "Pied De Vents Cheese",
+    price: "$5.16",
+    engine: "Canada",
+    image: "pied-de-vents.jpg"
+  },
+  {
+    id: 19,
+    name: "Sweet Potato Crackers",
+    price: "$6.81",
+    engine: "United States",
+    image: "crackers.jpg"
+  },
+  {
+    id: 20,
+    name: "Mushrooms",
+    price: "$8.62",
+    engine: "United States",
+    image: "mushrooms.jpg"
+  },
+  {
+    id: 21,
+    name: "Jerusalem Artichoke",
+    price: "$3.16",
+    engine: "United States",
+    image: "jerusalem-artichoke.jpg"
+  },
+  {
+    id: 22,
+    name: "Leather Leaf Fern",
+    price: "$9.41",
+    engine: "Brazil",
+    image: "leather-leaf.jpg"
+  },
+  {
+    id: 23,
+    name: "Dried Apricots",
+    price: "$7.42",
+    engine: "Brazil",
+    image: "dried-apricots.jpg"
+  },
+  {
+    id: 24,
+    name: "Navel Orange",
+    price: "$3.53",
+    engine: "Brazil",
+    image: "navel-orange.jpg"
+  },
+  {
+    id: 25,
+    name: "Sunflower Oil",
+    price: "$1.90",
+    engine: "Mexico",
+    image: "sunflower-oil.jpg"
+  },
+  {
+    id: 26,
+    name: "Black Turtle Beans",
+    price: "$4.67",
+    engine: "Canada",
+    image: "turtle-beans.jpg"
+  },
+  {
+    id: 27,
+    name: "Bananas",
+    price: "$2.64",
+    engine: "Canada",
+    image: "bananas.jpg"
+
+  },
+  {
+    id: 28,
+    name: "Salami",
+    price: "$8.72",
+    engine: "Brazil",
+    image: "salami.jpg"
+  },
+  {
+    id: 29,
+    name: "Proscuitto",
+    price: "$9.86",
+    engine: "Brazil",
+    image: "proscuitto.jpg"
+  },
+  {
+    id: 30,
+    name: "Cranberries",
+    price: "$5.85",
+    engine: "United States",
+    image: "cranberries.jpg"
+
+  },
+  {
+    id: 31,
+    name: "Canteloupe",
+    price: "$4.49",
+    engine: "United States",
+    image: "canteloupe.jpg"
+  },
+  {
+    id: 32,
+    name: "Lemon",
+    price: "$2.8Lemon5",
+    engine: "United States",
+    image: "lemon.jpg"
+  },
+  {
+    id: 33,
+    name: "Pesto",
+    price: "$2.67",
+    engine: "Brazil",
+    image: "pesto.jpg"
+  },
+  {
+    id: 34,
+    name: "Grapefruit Juice",
+    price: "$4.88",
+    engine: "Brazil",
+    image: "grapefruit-juice.jpg"
+
+  },
+  {
+    id: 35,
+    name: "Curry Paste",
+    price: "$2.02",
+    engine: "Brazil",
+    image: "curry-paste.jpg"
+  },
+  {
+    id: 36,
+    name: "Avocado Oil",
+    price: "$4.45",
+    engine: "Canada",
+    image: "avocado-oil.jpg"
+  },
+  {
+    id: 37,
+    name: "Queso Fresco",
+    price: "$9.24",
+    engine: "Canada",
+    image: "queso-fresco.jpg"
+  },
+  {
+    id: 38,
+    name: "Key Lime Pie",
+    price: "$5.32",
+    engine: "Mexico",
+    image: "key-lime-pie.jpg"
+  },
+  {
+    id: 39,
+    name: "Granny Smith Apple",
+    price: "$2.12",
+    engine: "United States",
+    image: "granny-smith.jpg"
+  },
+  {
+    id: 40,
+    name: "Morning Glory Muffin Mix",
+    price: "$9.22",
+    engine: "Brazil",
+    image: "muffin-mix.jpg"
+  },
+  {
+    id: 41,
+    name: "Heavy Whipping Cream",
+    price: "$8.78",
+    engine: "Brazil",
+    image: "heavy-cream.jpg"
+  },
+  {
+    id: 42,
+    name: "Red Bell Pepper",
+    price: "$9.03",
+    engine: "Brazil",
+    image: "red-bell-pepper.jpg"
+  },
+  {
+    id: 43,
+    name: "Perrier",
+    price: "$9.36",
+    engine: "United States",
+    image: "perrier.jpg"
+  },
+  {
+    id: 44,
+    name: "Yukon Gold Potatoes",
+    price: "$8.76",
+    engine: "Mexico",
+    image: "yukon-potatoes.jpg"
+  },
+  {
+    id: 45,
+    name: "Salmon Fillets",
+    price: "$6.21",
+    engine: "United States",
+    image: "salmon-fillets.jpg"
+  },
+  {
+    id: 46,
+    name: "Tilapia Fillets",
+    price: "$4.37",
+    engine: "Mexico",
+    image: "tilapia.jpg"
+  },
+  {
+    id: 47,
+    name: "Tomato Pesto",
+    price: "$9.70",
+    engine: "Brazil",
+    image: "tomato-pesto.jpg"
+  },
+  {
+    id: 48,
+    name: "Oil - Olive Bertolli",
+    price: "$8.92",
+    engine: "Brazil",
+    image: "olive-oil.jpg"
+  },
+  {
+    id: 49,
+    name: "Tear Drop Yellow Tomatoes",
+    price: "$3.32",
+    engine: "Mexico",
+    image: "yellow-tomatoes.jpg"
+  },
+  {
+    id: 50,
+    name: "Smoked Paprika",
+    price: "$8.31",
+    engine: "Brazil",
+    image: "smoked-paprika.jpg"
+  }
+]
+
+export default mock;
